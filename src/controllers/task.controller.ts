f8ed4e39f6025275813bca0409f2c8d856acb001
Juan Martinez <mcplux@outlook.com>
2026-01-05 15:20:30 -0500

    Add try catch to create task controller

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -6,12 +6,21 @@ export const createTask = async (
   req: Request<{}, {}, CreateTaskInput>,
   res: Response
 ) => {
-  const task = await Task.create({
-    ...req.body,
-  })
+  try {
+    const task = await Task.create({
+      ...req.body,
+    })
 
-  return res.json({
-    success: true,
-    data: task,
-  })
+    return res.json({
+      success: true,
+      data: task,
+    })
+  } catch (error) {
+    console.log('Error creating a task:', error)
+
+    return res.status(500).json({
+      success: false,
+      error: 'Something went wrong',
+    })
+  }
 }
